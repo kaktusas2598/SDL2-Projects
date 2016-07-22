b11e1785c14e06b1d99cee0c19e06e65cb773977
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2016-07-23 00:34:14 +0300

    Refactored test program.

--- main.cpp
+++ main.cpp
@@ -2,24 +2,26 @@
 
 SDL_Window * g_pWindow = 0;
 SDL_Renderer * g_pRenderer = 0;
+bool g_pRunning = false;
 
-int main(int argc, char* args[]){
+bool init(const char* title, int xpos, int ypos, int height, int width, int flags){
 
 	//init SDL
 	if(SDL_Init(SDL_INIT_EVERYTHING) >= 0){
 		//if succeed, create the window
-		g_pWindow = SDL_CreateWindow("Basic SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-				640, 480, SDL_WINDOW_SHOWN);
+		g_pWindow = SDL_CreateWindow(title , xpos, ypos, height, width, flags);
 
 		if(g_pWindow != 0){
 			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
 		}
 	}
 	else{
-		return 1; //could not init SDL
+		return false; //could not init SDL
 	}
+	return true;
+}
+void render(){
 
-	//All success, draw the window
 	//set window color (RGBA)
 	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
 
@@ -28,13 +30,23 @@ int main(int argc, char* args[]){
 
 	//show the window
 	SDL_RenderPresent(g_pRenderer);
+}
 
-	//set a delay before quitting
-	SDL_Delay(5000);
+int main(int argc, char* args[]){
 
+	if(init("SDL2 Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN)){
+		g_pRunning = true;
+	}
+	else{
+		return 1;//could not init
+	}
+
+	while(g_pRunning){
+		render();
+	}
+	
 	//clean up SDL
 	SDL_Quit();
 
 	return 0;
-
 }
