fe7fe6bdb22cfb4a225418aa6089b30c1be48218
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2016-07-23 15:29:45 +0300

    Added working example of basic Game class.

---
+++ GAME_CLASS/Game.h
@@ -0,0 +1,28 @@
+#ifndef __Game__
+#define __Game__
+
+#include <SDL2/SDL.h>
+
+class Game{
+	public:
+		Game() {}
+		~Game() {}
+
+		bool init(const char* title, int xpos, int ypos, int height, int width, int flags);
+
+		void render();
+		void update(){}
+		void handleEvents();
+		void clean();
+
+		bool running(){ return m_bRunning; }
+
+	private:
+
+		SDL_Window * m_pWindow = 0;
+		SDL_Renderer * m_pRenderer = 0;
+
+		bool m_bRunning;
+};
+
+#endif
---
+++ GAME_CLASS/main.cpp
@@ -0,0 +1,19 @@
+#include "Game.h"
+
+Game* g_game = 0;
+
+int main(int argc, char* args[]){
+
+	g_game = new Game();
+
+	g_game->init("Game Class", 100, 100, 640, 480, 0);
+
+	while(g_game->running()){
+		g_game->handleEvents();
+		g_game->update();
+		g_game->render();
+	}
+	g_game->clean();
+
+	return 0;
+}
